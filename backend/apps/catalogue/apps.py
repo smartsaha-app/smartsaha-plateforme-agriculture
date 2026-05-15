6a08ba38a2edf7fde3f7a1251832d34aea78acc5
mandroro <yvesaimable42@gmail.com>
2026-05-15 10:04:44 +0300

    (fix):Correction de bug redirection et automatisation du seed sur le categorie produit

--- backend/apps/catalogue/apps.py
+++ backend/apps/catalogue/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 class CatalogueConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.catalogue'
+
+    def ready(self):
+        import apps.catalogue.signals
